fee5082f291f87d73d3c523aa380ac7ed89276cb
Ahmed Znagui <amed.bob88@gmail.com>
2024-04-23 16:58:50 +0200

    next example

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import { useState } from "react";
 function App() {
   const [people, setPeople] = useState(data);
 
+  const reloadAllItem = () => {
+    setPeople(data)
+  }
+
+  const removeItem = (id) => {
+    setPeople((oldValue) => {
+      return oldValue.filter((value) => value.id !== id)
+    })
+  }
+
   return (
     <section>
       <div className="container">
@@ -13,12 +23,12 @@ function App() {
             prossimi incontri
         </h2>
          <div className="people-list">
-            <List data={people} />
+            <List data={people} removeItem = {removeItem} />
          </div>
 
          <div className="btn-group">
-            <button className="btn btn-reset">reset</button>
-            <button className="btn btn-delete">delete all</button>
+            <button className="btn btn-reset" onClick={reloadAllItem} >reset</button>
+            <button className="btn btn-delete" onClick={()=> setPeople([])}>delete all</button>
          </div>
       </div>
     </section>
--- src/List.js
+++ src/List.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {RiDeleteBack2Fill} from "react-icons/ri";
 
 const List = (props) => {
 
@@ -6,20 +7,23 @@ const List = (props) => {
   return <ul className="user-list">
     {props.data.map((person) =>(
       <li key={person.id}>
-        <Person {...person} />
+        <Person {...person} removeItem={props.removeItem} />
       </li>
     ))}
   </ul>;
 };
 
-const Person = ({id, stato, nome, img}) => {
+
+const Person = ({id, stato, nome, img, removeItem}) => {
+
+
   return <article>
     <img src={img} alt='prs' className="person-img"/>
     <div className="person-info">
 
     <div className="person-action">
         <h4>{nome}</h4>
-        <button className="btn btn-danger">delete</button>
+        <button className="btn btn-danger" onClick={() => removeItem(id)} > <RiDeleteBack2Fill className="icon" /> </button>
     </div>
     <p>{stato}</p>
     </div>
